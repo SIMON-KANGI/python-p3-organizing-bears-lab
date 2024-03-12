dbc410fadc13eea5de6a774b18bb626ebade365a
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-03-12 07:28:36 +0300

    completed

---
+++ lib/insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO bears(
+id,name,age, sex,color,temperament,alive
+);
+VALUES(
+1'Mr. Chocolate',3,"M","white","agressive",1
+2'Rowdy',4,"F","white","calm",1
+3'Tabitha',7,"F","BLACK","calm",0
+4'Sergeant Brown',2,"F","brown","agressive",1
+5'Melissa',3, "F","BLACK","playful",0
+6'Grinch', 6 , "M","BLACK","cal ",0
+7'Wendy',5,"F","Brown","clumsy",1
+);
